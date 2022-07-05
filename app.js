8fa13e9a80af8a901e3686e90d07632c1b5843e0
wifiantena <polad.eyyubov@gmail.com>
2022-07-05 16:02:13 +0200

    ui

---
+++ app.js
@@ -0,0 +1,12 @@
+//book list constructer
+//-------------------------------------------------------------
+function Book(title,author,isbn){
+this.title = title;
+this.author = author;
+this.isbn = isbn;
+
+function title(){
+  document.addEventListener('click')
+}
+}
+//---------------------------------------------------------------
